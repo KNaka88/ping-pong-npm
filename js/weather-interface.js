f90d37d3c329eb025fdc6b120be6be0e4bd17cff
Carlos Munoz Kampff and Koji Nakagawa <koji@>
2017-03-13 15:16:06 -0700

    add .then and .fail promises and error handling.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,13 +1,17 @@
-var apiKey = "YOUR-API-KEY-GOES-HERE";
+var apiKey = "";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-     $('.showWeather').append("The Temperture in  " + city + " is " + (parseFloat(response.main.temp)-273.15).toFixed(2) + "&#8451;");
-     $('.showWeather').append("The humidity in " + city + " is " + response.main.humidity + "%");
-   });
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response){
+        $('.showWeather').append("The Temperture in  " + city + " is " + (parseFloat(response.main.temp)-273.15).toFixed(2) + "&#8451;<br>");
+        $('.showWeather').append("The humidity in " + city + " is " + response.main.humidity + "%");
+      })
+      .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -25,16 +25,20 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "112eef42fc41c24630563ece8fffdf1b";
+var apiKey = "";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-     $('.showWeather').append("The Temperture in  " + city + " is " + (parseFloat(response.main.temp)-273.15).toFixed(2) + "&#8451;");
-     $('.showWeather').append("The humidity in " + city + " is " + response.main.humidity + "%");
-   });
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response){
+        $('.showWeather').append("The Temperture in  " + city + " is " + (parseFloat(response.main.temp)-273.15).toFixed(2) + "&#8451;<br>");
+        $('.showWeather').append("The humidity in " + city + " is " + response.main.humidity + "%");
+      })
+      .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
